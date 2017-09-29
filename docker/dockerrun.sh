5b5ce696aeaa7de5bbf665ff734e971978c66509
Miguel Ponce de Leon <miguelpdl@tssg.org>
2017-09-29 16:33:16 +0100

    Adding in the SSL config.

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -6,6 +6,10 @@
 # Give the container a meaningful name
 NAME_SERVO=servo-msgbroker
 
+sudo ln -s /etc/letsencrypt/live/servomsgbroker.tssg.org/cert.pem $(pwd)/../src/cloud/conf/cert.pem
+sudo ln -s /etc/letsencrypt/live/servomsgbroker.tssg.org/chain.pem $(pwd)/../src/cloud/conf/chain.pem
+sudo ln -s /etc/letsencrypt/live/servomsgbroker.tssg.org/privkey.pem $(pwd)/../src/cloud/conf/privkey.pem
+
 # Run the main container.
 docker run \
     --name $NAME_SERVO \
@@ -13,5 +17,8 @@ docker run \
     -u `id -u $USER` \
     -v $(pwd)/../src/cloud/conf/mosquitto.conf:/mosquitto/config/mosquitto.conf \
     -v $(pwd)/../src/cloud/conf/passwd:/mosquitto/config/passwd \
+    -v $(pwd)/../src/cloud/conf/cert.pem:/mosquitto/config/cert.pem \
+    -v $(pwd)/../src/cloud/conf/chain.pem:/mosquitto/config/chain.pem \
+    -v $(pwd)/../src/cloud/conf/privkey.pem:/mosquitto/config/privkey.pem \
     -v $(pwd)/../src/cloud/log/:/mosquitto/log/ \
     -d -t servo/mosquitto
